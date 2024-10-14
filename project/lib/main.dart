3639ea67f28d2974d2d634cb879d28a9e646c66c
Woojin Song <woojin1900@gmail.com>
2024-10-14 20:56:25 +0900

    Stack 사용하여 여러 위젯 중첩하기

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -50,6 +50,30 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
       ),
       body: Container(
+        width: MediaQuery.of(context).size.width,
+        color: Colors.yellow,
+        child: Stack(
+          children: [
+            Container(
+              width: 200,
+              height: MediaQuery.of(context).size.height,
+              color: Colors.blue,
+            ),
+            Container(
+              width: 200,
+              height: 200,
+              margin: EdgeInsets.only(top: 50, left: 50),
+              color: Colors.red,
+            ),
+            Container(
+              width: 200,
+              height: 200,
+              margin: EdgeInsets.only(top: 100, left: 100),
+              color: Colors.green,
+            ),
+          ],
+        ),
+        /*
         alignment: Alignment.topLeft,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -86,6 +110,7 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ],
         ),
+        */
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => print("Cliked"),
